78d790815406a6f93beb62e933dfbcee0d417759
Clayton Cook <claytoncook@o-4.local>
2019-08-06 22:04:57 -0400

    Download Playlist Videos

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import tkinter
 import os
-import subprocess
 import colors as c
 
-# Example Video:
-# https://www.youtube.com/watch?v=-1EPegIzD2M&list=LLgugZespId8haUhw5Wuwt5g&index=29&t=0s
+window = tkinter.Tk()
+window.title("Youtube Video Downloader")
 
-def brew():
+downloadType = tkinter.IntVar()
+downloadType.set(1)
+link = tkinter.StringVar()
+
+videoEntry = tkinter.Entry(window, textvariable=link)
+
+def downloadVideo():
+    print(downloadType.get(), link.get())
+
+    if downloadType.get() == 1 and not(link.get() == ""):
+        os.chdir("output")
+        os.system("youtube-dl -f best " + str(link.get()))
+    elif downloadType.get() == 2 and not(link.get() == ""):
+        os.chdir("output")
+        os.system("youtube-dl --yes-playlist -f best " + str(link.get()))
+    else:
+        c.printColor(c.red, "Please choose a valid option.")
+
+    os.chdir("..")
+
+submitButton = tkinter.Button(window, text="Submit", command=downloadVideo)
+
+def install():
     checkIfBrewIsInstalled = str(os.system("brew -v")).split()
     if checkIfBrewIsInstalled[0] == "0":
         c.printColor(c.blue, "Brew is installed!")
@@ -15,35 +36,22 @@ def brew():
         print("Installing now... \n")
         os.system('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
 
-def youtubeDL():
-    chechIfYoutubeDLIsInstalled = str(os.system("youtube-dl"))
-    print(chechIfYoutubeDLIsInstalled)
-    if chechIfYoutubeDLIsInstalled == "0":
+    checkIfYoutubeDLIsInstalled = str(os.system("youtube-dl"))
+    if checkIfYoutubeDLIsInstalled == "0":
         c.printColor(c.blue, "Youtube-DL is installed!")
     else:
         c.printColor(c.red, "Youtube-DL is not installed!")
         print("Installing now... \n")
         os.system("brew install youtube-dl")
 
-def downloadVideo():
-    l = link.get()
-    print(l)
-    os.system("youtube-dl " + str(l))
-
-window = tkinter.Tk()
-window.title("Youtube Video Downloader")
-link = tkinter.StringVar()
-videoEntry = tkinter.Entry(window, textvariable=link)
-
-submitButton = tkinter.Button(window, text="Submit", command=downloadVideo)
-
 def main():
-    brew()
-    youtubeDL()
+    install()
 
-    tkinter.Label(window, text="Video: ").grid(row=0)
-    videoEntry.grid(row=0, column=1)
-    submitButton.grid(row=1)
+    tkinter.Radiobutton(window, text="Video", variable=downloadType, value=1).grid(row=0)
+    tkinter.Radiobutton(window, text="Playlist", variable=downloadType, value=2).grid(row=0,column=1)
+    tkinter.Label(window, text="Video: ").grid(row=1)
+    videoEntry.grid(row=1, column=1)
+    submitButton.grid(row=2)
     window.mainloop()
 
 main()
